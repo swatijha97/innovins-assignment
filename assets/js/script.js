4d77500ad8702fec8eb226b44142f65629c0e3b0
Swati Jha <jha.swati.1797@gmail.com>
2022-05-24 13:17:45 +0530

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    margin: 30,
+    loop: false,
+    items: 3,
+    nav: false,
+    dots: false,
+    stagePadding: 80,
+  });
+});
